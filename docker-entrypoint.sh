bc4b456c819f5cfbf370c75be6cdcf03eb508784
Mugen0815 <dennis@_HOSTNAME_>
2023-10-20 20:53:57 +0200

    FIXED: apache-conf. ADDED: composer install for existing projects. CHANGED: Dockerfile optimized

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,11 @@ if [[ "$1" == apache2* ]] || [ "$1" = 'php-fpm' ]; then
     if [ ! -e /usr/src/app/public/index.php ]; then
         echo "CREATING PROJECT"
         /usr/bin/symfony new /usr/src/app --version='6.3.*' --webapp
+    else
+        if [ ! -e /usr/src/app/vendor ]; then
+            echo "INSTALLING DEPENDENCIES"
+            cd /usr/src/app && composer install
+        fi    
     fi
 fi
 
